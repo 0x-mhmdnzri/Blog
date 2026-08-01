ca74ff79e21f72b3bf1dab7e0e66e1372a7feba3
Mohammad <nazari.mohammad80@icloud.com>
2026-08-01 16:42:13 +0330

    fix(di): register IEnterpriseService for AdminEnterprise

---
+++ BlogApp/Services/Enterprise/EnterpriseStartup.cs
@@ -0,0 +1,10 @@
+namespace BlogApp.Services.Enterprise;
+
+public static class EnterpriseStartup
+{
+    public static IServiceCollection AddEnterpriseServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEnterpriseService, EnterpriseService>();
+        return services;
+    }
+}
