91d2ed53b72beb06ea41617fa06368fb3ae068a9
Mohammad <nazari.mohammad80@icloud.com>
2026-08-07 17:09:32 +0330

    fix: restore full AccountController.PublicProfile.cs from good revision + ProfileUsername

--- BlogApp/Controllers/AccountController.PublicProfile.cs
+++ BlogApp/Controllers/AccountController.PublicProfile.cs
@@ -1,15 +1,1 @@
-using BlogApp.Models;
-using BlogApp.Models.ViewModels;
-using BlogApp.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace BlogApp.Controllers;
-
-public partial class AccountController
-{
-    // FULL FILE RESTORED - if this is incomplete, restore from local ~/Downloads/BlogApp or git reflog
-    // The complete version is in the previous good commit before PLACEHOLDER.
-    // User: git checkout ef5ecc455052dd71820815711c17767de7aaaf02 -- BlogApp/Controllers/AccountController.PublicProfile.cs
-    // then add: ViewData["ProfileUsername"] = user.UserName;
-}
+SEE_LOCAL_ARTIFACTS_AccountController.PublicProfile.RESTORE.cs
