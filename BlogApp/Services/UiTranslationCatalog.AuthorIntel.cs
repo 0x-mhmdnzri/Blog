2016a7e3805460674290c4c8e1d732845f5ee79b
Mohammad <nazari.mohammad80@icloud.com>
2026-08-07 15:07:20 +0330

    feat(admin): Author Intelligence translation keys

---
+++ BlogApp/Services/UiTranslationCatalog.AuthorIntel.cs
@@ -0,0 +1,9 @@
+namespace BlogApp.Services;
+
+public static partial class UiTranslationCatalog
+{
+    public static readonly (string Key, string Group, string Fa, string En, string Ar)[] AuthorIntel =
+    {
+        ("admin.nav.author_intel", "admin", "هوش نویسندگان", "Author Intelligence", "ذكاء المؤلفين"),
+    };
+}
